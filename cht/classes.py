656b3317c1136465e2827e214cc55ebcdf591ec8
ConradSelig <conrad.selig@mines.sdsmt.edu>
2019-08-26 16:31:28 -0600

    Shifting of functions in classes.py and in-code documentation

--- cht/classes.py
+++ cht/classes.py
@@ -1,10 +1,12 @@
-from datetime import datetime
 from cht import hashing
 from cht import output
 import re
 
 
 class CHT:
+    """
+        CHT holds both the raw table data (built into classes) and the compiled cubic hash table itself.
+    """
 
     def __init__(self, keys, values):
         self.keys = keys + ["__words__"]
@@ -28,34 +30,50 @@ class CHT:
     def get_keys(self):
         return self.keys
 
-    def do_hash(self, show_print=True):
-        self.add_rows([], True, show_print)
+    def do_hash(self, show_print=False):
 
-    def add_row(self, row, do_hash=True, show_print=True):
+        if show_print:
+            print("Building Cubic Hash Table...")
+        # for key in CHT
+        for i, key in enumerate(self.keys):
+            if show_print:
+                output.progress_bar(i, len(self.keys))
+            # build that keys hash table
+            self.hash_map_dict[key] = hashing.build_hash_table(self, key)
+        if show_print:
+            output.progress_bar(len(self.keys), len(self.keys))
+
+        return
+
+    def add_row(self, row, do_hash=True, show_print=False):
         self.add_rows([row], do_hash, show_print)
         return
 
-    def add_rows(self, rows, do_hash=True, show_print=True):
+    def add_rows(self, rows, do_hash=True, show_print=False):
+        # for each row in new data
         for row in rows:
+            # create a new Row object with the table keys
             temp_row = Row(self.keys)
-            temp_row.build_from_event(row)
+            # build the new row object with the table data
+            temp_row.build_from_table(row)
+            # append to row to the CHT
             self.rows.append(temp_row)
+            # for each word in the new row
             for new_word in temp_row.get_words():
+                # append each word to the CHT as a Word object
                 self.words.append(Word(new_word, temp_row.get("id")))
+        # for each row in the CHT raw rows
         for row in self.rows:
+            # if an ID was not found in the row or a blank ID was givin
             if row.get("id") == -1 or row.get("id") == "":
+                # re-id the entire CHT, this operation is usually overkill but the CHT *requires* unique IDs and this
+                # is the best way to ensure that
                 for new_id, id_row in enumerate(self.rows):
                     id_row.set_id(new_id)
                 break
+        # if a new hash needs to be made
         if do_hash:
-            if show_print:
-                print("Building Cubic Hash Table...")
-            for i, key in enumerate(self.keys):
-                if show_print:
-                    output.progress_bar(i, len(self.keys))
-                self.hash_map_dict[key] = hashing.build_hash_table(self, key)
-            if show_print:
-                output.progress_bar(len(self.keys), len(self.keys))
+            self.do_hash(show_print)
 
         return
 
@@ -64,18 +82,25 @@ class CHT:
 
 
 class Word:
+    """
+     The word class is only used in the __words__ array where the row index is no longer associated with the word itself
+        and therefor that index must be stored with the word.
+    """
 
     def __init__(self, word, word_id):
         self.word = word
         self.id = word_id
 
+    # this function is an override of the getattr() function that allows for use as a class function
     def get(self, attr):
-        if attr == "id":
-            return self.id
-        return self.word
+        return getattr(self, attr)
 
 
 class Row:
+    """
+    The row class holds RAW data only, hash requests cannot be made to the row class.
+    For the hashed version of the row class see the HashMapVal class.
+    """
 
     def __init__(self, attrs):
         self.attrs = attrs
@@ -91,7 +116,7 @@ class Row:
         foreign = [str(getattr(other, key)) for key in [attr for attr in self.attrs if "__" not in attr]]
         return local == foreign
 
-    def build_from_event(self, values):
+    def build_from_table(self, values):
         for index, key in enumerate(self.attrs):
             try:
                 if key == "id":
@@ -150,6 +175,10 @@ class Row:
 
 
 class HashMapValue:
+    """
+    The CHT is built out of instances of this class. Hash requests should never be made to this class and should always
+    be made through the CHT holding this classes objects.
+    """
 
     def __init__(self):
         self.key = ""
