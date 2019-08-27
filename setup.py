bb506feb03ddafe7246ceb812d88c96eeecee170
ConradSelig <conrad.selig@mines.sdsmt.edu>
2019-08-27 14:07:10 -0600

    Swapping " with ' and version bump

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name="CHT",
-    version="0.0.3",
-    packages=[""],
-    url="https://github.com/ConradSelig/CHT",
-    license="Apache Licence 2.0",
-    author="Conrad Selig",
-    author_email="conrad.selig@mines.sdsmt.edu",
-    description="CHT (Cubic Hash Table) is a new data structure allowing instant lookup on table table,"
-                "with special focus on primitive data lookup."
+    name='CHT',
+    version='0.0.4',
+    packages=[''],
+    url='https://github.com/ConradSelig/CHT',
+    license='Apache Licence 2.0',
+    author='Conrad Selig',
+    author_email='conrad.selig@mines.sdsmt.edu',
+    description='CHT (Cubic Hash Table) is a new data structure allowing instant lookup on table table,'
+                ' with special focus on primitive data lookup.'
 )
